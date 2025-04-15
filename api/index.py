c6633b0e3bb3567384cccf427fc4b5c0a882f8f8
mokasp <kasperbouldin@gmail.com>
2025-04-15 18:33:01 -0500

    (fix) add correct model format

--- api/index.py
+++ api/index.py
@@ -1,15 +1,19 @@
-from flask import Flask, jsonify
-import tensorflow as tf
+from flask import Flask, jsonify, request
+import tflite_runtime.interpreter as tflite
 import logging
 
 app = Flask(__name__)
+
+interpreter = tflite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 @app.route("/")
 def home():
     return 'Hello, World! :)'
 
-@app.route("/predict")
+@app.route("/predict", methods=["POST"])
 def predict():
-    model = tf.keras.models.load_model('api/model/test_model_00.keras')
     logging.debug("model loaded!")
     return 'Prediction coming'
